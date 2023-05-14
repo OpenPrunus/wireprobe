843bffe7ddb9a3592d8d03efc9a47b37b5f80a6f
Prunus <prunus@Elliot.home>
2023-05-14 15:59:34 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='wireprobe',
-      version='0.1.4',
+      version='0.1.5',
       description='A wireguard probe',
       long_description="This package monitor wireguard client's tunnel if is up or down",
       author='Benjamin Gounine',
@@ -11,4 +11,5 @@ setup(name='wireprobe',
       url='https://github.com/OpenPrunus/wireprobe/',
       python_requires='>3.9',
       install_requires=["decorator", "fabric", "invoke", "pyyaml", "urllib3"],
+      include_package_data=True,
       )
