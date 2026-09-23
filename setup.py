e805b35615c26515efc6f6bdb94a79a14c38ade6
Prunus <prunus@ecuri.es>
2026-09-23 21:54:49 +0200

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='wireprobe',
-      version='0.2',
+      version='0.3',
       description='A wireguard app',
       long_description="This package monitor wireguard client's tunnel if is up or down",
       author='Benjamin Gounine',
